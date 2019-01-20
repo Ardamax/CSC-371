d89a25682dafa958bda28ad479b8a6d00d9d9b3e
rocketeer55 <spencer.schurk@gmail.com>
2019-01-19 20:38:09 -0800

    Added basic player and mechanics, and moving background

---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalMovement, verticalMovement) * speed;
+        rb2d.AddForce(movement);
+
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+    }
+}
