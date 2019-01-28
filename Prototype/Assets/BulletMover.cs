2c7d8cfd0e7f075073c6bab49db431214b9b1cb6
Spencer Schurk <spencer@joyrun.com>
2019-01-28 12:02:21 -0800

    Added bullets

---
+++ Prototype/Assets/BulletMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    public float speed;  
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector3(0f, 1f, 0f) * speed * Time.deltaTime);
+    }
+}
---
+++ Prototype/Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float spawnRate = 0.1f;
+    public GameObject bulletPrefab;
+    private KeyCode key;
+    private float timeSinceLastSpawned = 0f;
+    private Vector2 spawnOffset = new Vector2(0f, 0.5f);
+
+    void Start() {
+        if (gameObject.CompareTag("Left Wing")) {
+            key = KeyCode.Q;
+        }
+        else if (gameObject.CompareTag("Right Wing")) {
+            key = KeyCode.E;
+        }
+        else {
+            // idk
+        }
+    }
+
+    void Update() {
+        timeSinceLastSpawned += Time.deltaTime;
+        if (Input.GetKeyDown(key) && timeSinceLastSpawned > spawnRate) {
+            Instantiate(bulletPrefab, new Vector2(gameObject.transform.position.x + spawnOffset.x, gameObject.transform.position.y + spawnOffset.y), Quaternion.identity); 
+        }
+    }
+}
