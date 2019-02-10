fb36d92e6d23fad30dbc2a1b8ec83ec227d9bf63
nauticalmonkey <jevrem.kboskovic@gmail.com>
2019-02-09 18:06:10 -0800

    Add files via upload

---
+++ Prototype/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Justin Evnas
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime = 5f; // we can change these as we please		
+	public float spawnDelay = 3f;		
+	public GameObject[] enemies;	
+
+
+	void Start ()
+	{
+		
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+
+
+	void Spawn ()
+	{
+		
+		int enemyIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+
+	}
+}
