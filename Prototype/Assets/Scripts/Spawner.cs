a0252a759350bd1fce02fd375f8b4959775747da
Justin Evans <Justin Evans>
2019-02-27 19:02:16 -0800

    added boss element

--- Prototype/Assets/Scripts/Spawner.cs
+++ Prototype/Assets/Scripts/Spawner.cs
@@ -7,18 +7,48 @@ public class Spawner : MonoBehaviour
 {
 	public float spawnTime = 5f; // we can change these as we please		
 	public float spawnDelay = 3f;		
-	public GameObject[] enemies;	
+	public GameObject[] enemies;
+
+    public GameObject Boss;
+    public GameObject[] baddies;
+    public float BossSpawnsIn = 120.0f;
 
 
-	void Start ()
+
+
+    void Start ()
 	{
 		InvokeRepeating("Spawn", spawnDelay, spawnTime);
 	}
 
 
-	void Spawn ()
+    void fixedupdate()
+    {
+        BossSpawnsIn -= Time.deltaTime;
+        if (BossSpawnsIn < 0)
+        {
+            Debug.Log("THIS IS HAPPENEING");
+            BossSpawn();
+        }
+
+    }
+
+
+    void Spawn ()
 	{
 		int enemyIndex = Random.Range(0, enemies.Length);
 		Instantiate(enemies[enemyIndex]);
 	}
+
+
+    void BossSpawn()
+    {
+        int i = 0;
+        Instantiate(Boss);
+        while(i< baddies.Length)
+        {
+            Instantiate(baddies[i]);
+            i++;
+        }
+    }
 }
