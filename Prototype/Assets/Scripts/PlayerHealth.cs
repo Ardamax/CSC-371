83247d5a6cc3b2c82c5071aaf997918514336389
Justin Evans <jevrem.kboskovic@gmail.com>
2019-03-07 13:58:21 -0800

    movment of the battleships

--- Prototype/Assets/Scripts/PlayerHealth.cs
+++ Prototype/Assets/Scripts/PlayerHealth.cs
@@ -67,7 +67,7 @@ public class PlayerHealth : MonoBehaviour
         {
             if (sceneName == "Level1")
             {
-                sceneName == "NotLevel1";
+                sceneName = "NotLevel1";
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
             else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
@@ -96,7 +96,7 @@ public class PlayerHealth : MonoBehaviour
 	public void UpdateHealthBar ()
 	{
 		// Set the health bar's colour to proportion of the way between green and red based on the player's health.
-		healthBar.material.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.1f);
+		healthBar.material.color = Color.Lerp(Color.blue, Color.red, 1 - health * 0.1f);
 
 		// Set the scale of the health bar to be proportional to the player's health.
 		healthBar.transform.localScale = new Vector3(healthScale.x * health * 0.1f, 1, 1);
