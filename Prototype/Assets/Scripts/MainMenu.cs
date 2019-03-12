c2dfc627c81dfedea0e6779a170b493d9fcee38c
Justin Evans <jevrem.kboskovic@gmail.com>
2019-03-12 15:49:13 -0700

    pause is now in the game

--- Prototype/Assets/Scripts/MainMenu.cs
+++ Prototype/Assets/Scripts/MainMenu.cs
@@ -16,6 +16,7 @@ public class MainMenu : MonoBehaviour
     }
     public void ReturnToMainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 }
--- Prototype/Assets/Scripts/Pause.cs
+++ Prototype/Assets/Scripts/Pause.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
@@ -13,26 +14,28 @@ public class Pause : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!pausePanel.activeInHierarchy)
-            {
                 PauseGame();
-            }
-            if (pausePanel.activeInHierarchy)
-            {
-                ContinueGame();
-            }
+            //if (pausePanel.activeInHierarchy)
+            //{
+            //    ContinueGame();
+           // }
         }
     }
     private void PauseGame()
     {
         Time.timeScale = 0;
         pausePanel.SetActive(true);
-        //Disable scripts that still work while timescale is set to 0
     }
     private void ContinueGame()
     {
         Time.timeScale = 1;
         pausePanel.SetActive(false);
         //enable the scripts again
+    }
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        SceneManager.LoadScene(0);
     }
 }
