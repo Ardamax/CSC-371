6641e4bcca00f4e03890d0161917596bb4ba6a67
Justin Evans <Justin Evans>
2019-03-17 00:55:13 -0700

    fixed bugs, balanced levels, added choice, So much meaningful play

--- Prototype/Assets/Scripts/MainMenu.cs
+++ Prototype/Assets/Scripts/MainMenu.cs
@@ -9,6 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+    public void FortheHorde()
+    {
+        SceneManager.LoadScene(15);
+    }
     public void QuitGame()
     {
         Debug.Log("Quit");
